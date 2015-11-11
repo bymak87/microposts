38419c397b16228192353ded23b8943a2185eb73
Amy Bakameyer <bymak87@gmail.com>
2015-11-11 10:06:25 -0600

    finalized project

--- client/common.js
+++ client/common.js
@@ -1,8 +1,23 @@
-Meteor.startup(function (){
+Meteor.subscribe("posts");
+Meteor.subscribe("ProfileImages");
+Meteor.subscribe("UserImages");
+
+
+Meteor.startup(function () {
   AccountsEntry.config({
     wrapLinks: true,
     homeRoute:'/',
     dashboardRoute: '/',
+    passwordSignupFields: 'USERNAME_AND_EMAIL',
     waitEmailVerification: false
   });
+
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+  });
+});
+
+Template.registerHelper('getProfileImg', function (userId){
+  var imgUrl = UserImages.findOne({userId: userId}).image;
+  return imgUrl;
 });
--- client/templates/profile/profile.js
+++ client/templates/profile/profile.js
@@ -1,27 +1,28 @@
 Template.profile.events({
-  "submit .edit-profile": function(event){
-    //get(0) for first field
-    var file = $("#profileImage").get(0).files[0];
+	"submit .edit-profile": function(event){
+		var file = $('#profileImage').get(0).files[0];
 
-    if(file){
-      fsFile = new FS.File(file);
+		if(file){
+			fsFile = new FS.File(file);
 
-      ProfileImages.insert(fsFile, function (err, result){
-        if (err){
-          throw new Meteor.Error(err);
-        } else {
-          var imageLoc = '/cfs/files/ProfileImages/'+result._id;
+			ProfileImages.insert(fsFile, function(err, result){
+				if(err){
+					throw new Meteor.Error(err);
+				} else {
 
-          UserImages.insert({
-            userId: Meteor.userId(),
-            username: Meteor.user().username,
-            image: imageLoc
-          });
-          Router.go('/');
-        }
-      });
-    }
+					var imageLoc = '/cfs/files/ProfileImages/'+result._id;
 
-    return false;
-  }
+					UserImages.insert({
+						userId: Meteor.userId(),
+						username: Meteor.user().username,
+						image: imageLoc
+					});
+
+					Router.go('/');
+				}
+			});
+		}
+
+		return false;
+	}
 });
